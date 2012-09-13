3032621cebabf00ca91ff2b41c9b4c6e640a39f9
Ricardo Neri <ricardo.neri@ti.com>
2012-09-13 12:29:05 -0500

    GPIO: PCA953x: Add DT match table
    Signed-off-by: Ricardo Neri <ricardo.neri@ti.com>

--- drivers/gpio/gpio-pca953x.c
+++ drivers/gpio/gpio-pca953x.c
@@ -789,6 +789,7 @@ static const struct of_device_id pca953x_dt_ids[] = {
 	{ .compatible = "ti,tca6408", },
 	{ .compatible = "ti,tca6416", },
 	{ .compatible = "ti,tca6424", },
+	{ .compatible = "ti,tca6424a-gpio", },
 	{ }
 };
 
