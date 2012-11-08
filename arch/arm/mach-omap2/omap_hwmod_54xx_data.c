6552d3db3d04f5462165ed076b8628d6ff2778c5
Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
2012-11-07 18:44:22 -0600

    ARM: OMAP5: hwmod: add bus-error back flag to iommu attributes
    The bus-error back response flag information has been added to
    the iommu device attributes for DSP and IPU processors on OMAP5.
    
    The flag is not relevant for dsp, and has been therefore set to 0.
    
    Signed-off-by: Subramaniam Chanderashekarapuram <subramaniam.ca@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/mach-omap2/omap_hwmod_54xx_data.c
+++ arch/arm/mach-omap2/omap_hwmod_54xx_data.c
@@ -2518,9 +2518,10 @@ static struct omap_hwmod_class omap54xx_mmu_hwmod_class = {
 /* ipu mmu */
 
 static struct omap_mmu_dev_attr ipu_mmu_dev_attr = {
-	.da_start = 0x0,
-	.da_end = 0xfffff000,
-	.nr_tlb_entries = 32,
+	.da_start 		= 0x0,
+	.da_end 		= 0xfffff000,
+	.nr_tlb_entries 	= 32,
+	.has_bus_err_back 	= 1,
 };
 
 static struct omap_hwmod omap54xx_ipu_mmu_hwmod;
@@ -2573,9 +2574,10 @@ static struct omap_hwmod omap54xx_ipu_mmu_hwmod = {
 /* dsp mmu */
 
 static struct omap_mmu_dev_attr dsp_mmu_dev_attr = {
-	.da_start = 0x0,
-	.da_end = 0xfffff000,
-	.nr_tlb_entries = 32,
+	.da_start 		= 0x0,
+	.da_end 		= 0xfffff000,
+	.nr_tlb_entries 	= 32,
+	.has_bus_err_back 	= 0,
 };
 
 static struct omap_hwmod omap54xx_dsp_mmu_hwmod;
