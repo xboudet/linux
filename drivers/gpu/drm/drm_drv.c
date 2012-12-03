b0721abc1565ec3c3b160c24662745045e1704c8
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-12-03 13:45:48 +0000

    drm_drv: tone logs down a bit
    We don't really need to know when something succeeded repeatedly,
    so only log failure return codes.

--- drivers/gpu/drm/drm_drv.c
+++ drivers/gpu/drm/drm_drv.c
@@ -480,12 +480,13 @@ long drm_ioctl(struct file *filp,
 	if (kdata != stack_kdata)
 		kfree(kdata);
 	atomic_dec(&dev->ioctl_count);
-	if (retcode)
+	if (retcode) {
 		DRM_DEBUG("pid=%d, cmd=0x%02x, nr=0x%02x, dev 0x%lx, auth=%d\n",
 			  task_pid_nr(current), cmd, nr,
 			  (long)old_encode_dev(file_priv->minor->device),
 			  file_priv->authenticated);
 		DRM_DEBUG("ret = %x\n", retcode);
+	}
 	return retcode;
 }
 
