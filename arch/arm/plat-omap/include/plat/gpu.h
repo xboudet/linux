b4f47c0c2edcb4b1316580aa1355aa4df65f1d1d
Vincent Stehlé <v-stehle@ti.com>
2013-01-31 16:50:40 +0100

    Hack path to fix compilation
    Signed-off-by: Vincent Stehlé <v-stehle@ti.com>

--- arch/arm/plat-omap/include/plat/gpu.h
+++ arch/arm/plat-omap/include/plat/gpu.h
@@ -19,7 +19,8 @@
 #ifndef OMAP_GPU_H
 #define OMAP_GPU_H
 
-#include <plat/omap-pm.h>
+#include "../arch/arm/mach-omap2/omap-pm.h"
+
 #include <linux/platform_device.h>
 
 struct gpu_platform_data {
